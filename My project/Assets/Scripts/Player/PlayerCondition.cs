07893c9ee70d2079a143275f456cbfc6e4efdf8a
yj6662 <yj6662@naver.com>
2025-05-20 11:00:18 +0900

    [feat] PlayerControl
    -feat PlayerControl
    -feat PlayerUI

---
+++ My project/Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public Condition health;
+    public Condition stamina;
+
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        if (stamina != null)
+        {
+            stamina.Add(stamina.passiveValue * Time.deltaTime);
+        }
+
+        if (health != null)
+        {
+            health.Add(health.passiveValue * Time.deltaTime);;
+        }
+
+        if (health.curValue <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        if (health != null)
+        {
+            health.Add(amount);
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (health != null)
+        {
+            health.Subtract(amount);
+        }
+        onTakeDamage?.Invoke();
+    }
+
+    public void Die()
+    {
+        Debug.Log("사망!");
+        //TODO: 체크포인트에서 리스폰
+    }
+}
+
